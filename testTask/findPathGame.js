51527c46e5fe7168fc8fb02f75660e5aedac8479
GrinalRakholiya <grinal.rakholiya@exhibytesolution.com>
2024-01-31 18:14:40 +0530

    given matrix of integers representing the grid of streets, where each integer corresponds to a different type of road square and check  if it's possible to make your way to the exit though that matrix

--- testTask/findPathGame.js
+++ testTask/findPathGame.js
@@ -4,48 +4,77 @@ const direction = {
     2: [4, 3],
     3: [2, 3],
     4: [1, 2],
-    5: [1, 4],
+    5: [4, 1],
 };
 const matrix = [
-    [0, 2, 0, 2],
-    [0, 1, 0, 1],
-    [0, 4, 0, 0],
+    // [0, 2, 0, 2],
+    // [0, 1, 0, 1],
+    // [0, 4, 0, 0],  
+
+    [2, 3, 2],
+    [1, 1, 1],
+    [1, 1, 1],
+    [4, 5, 4]
 ];
-function correctInput(startDirection, finalExit) {
-    const validMoves = {
-        1: [3],
-        3: [1],
-        4: [2],
-        2: [4],
-        5: [1]
-    };
-    return validMoves[finalExit] && validMoves[finalExit].includes(startDirection);
-}
+
+const validMoves = {
+    1: 3,
+    3: 1,
+    4: 2,
+    2: 4,
+    5: 1,
+};
+
 function canMove(matrix, x = 0, y = 0, lastMove = 2, moves = []) {
-    const value = matrix[x][y];
-    const [start, end] = direction[value];
-    if (!correctInput(start, lastMove)) {
+    const first = matrix[0][0];
+
+    const last = matrix[matrix.length - 1][matrix[0].length - 1];
+
+    if (!(first === 0 || first === 2) || !(last === 0 || last === 4)) {
         return false;
     }
+
+    const value = matrix[x][y];
+    console.log(value)
+
+    if (x === matrix.length - 1 || y === matrix[0].length - 1) {
+        if (value === undefined) {
+            return end;
+        }
+    }
+    let [start, end] = direction[value];
+    const startIndex = [start, end].indexOf(validMoves[lastMove]);
+
+    //console.log("startIndex ", startIndex);
+
+    if (startIndex < 0) {
+        return false;
+    }
+
+    end = [start, end][[startIndex == 0 ? 1 : 0]];
+
     lastMove = end;
+
     moves.push(value);
     if (x === matrix.length - 1 && y === matrix[0].length - 1) {
         console.log("Moves:", moves);
-        return true;
+
     }
-    if (end === 0 || end === 3 || end === 4) {
-        x++;
-    } else if (end === 1 || end === 2) {
-        y++;
-    } else if (end === 5) {
+
+    if (end === 1) {
         x--;
+    } else if (end === 2) {
+        y++;
+    } else if (end === 3) {
+        x++;
+    }
+    else if (end === 4) {
+        y--;
     }
     if (x >= 0 && x < matrix.length && y >= 0 && y < matrix[0].length) {
         return canMove(matrix, x, y, lastMove, moves);
-    } else {
-        return false;
     }
+
 }
-if (!canMove(matrix)) {
-    console.log("Cannot reach exit.");
-}
+
+console.log(canMove(matrix));
